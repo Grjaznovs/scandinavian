1dc426325de163a0d215db0ea50f4b96e3014fde
Grjaznovs <44875274+Grjaznovs@users.noreply.github.com>
2020-08-29 15:48:43 +0300

    Create web.php

---
+++ web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('setlocale/{locale}', function ($locale) {
+	if (in_array($locale, Config::get('app.locales'))) {
+		Session::put('locale', $locale);
+	}
+	return redirect()->back();
+});
+
+Route::resource('scandinavian/world/test', 'ScandinavianWorldController');
