1fdfd0ddc1691c2cfd777c4e2b539d477e0bd857
Grjaznovs <44875274+Grjaznovs@users.noreply.github.com>
2020-08-29 15:52:08 +0300

    Create scandinavian.php

---
+++ scandinavian.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'modal-list-title' => 'Document upload window',
+	'title-file' => 'Upload a file',
+	'check-file' => 'Select a file',
+	'title-modal' => 'Document',
+
+	'btn-add' => 'Upload',
+	'btn-cencel' => 'Cancel',
+		
+	'msg_success_file_upload' => 'File successfully attached',
+	'msg_error_db' => 'Error! Unable to save changes',
+	'msg_empty' => 'Field cannot be empty',
+	'msg_not_found' => 'File not found!',
+];
